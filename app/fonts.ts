53e0310329a8732a80d4f5e1bc83f532f2b6a889
Santiago Quiceno Silva <santiagoquicenosilva@Santiagos-MacBook-Pro.local>
2025-10-27 20:06:38 -0500

    Fix local font paths

--- app/fonts.ts
+++ app/fonts.ts
@@ -2,12 +2,12 @@ import localFont from "next/font/local";
 
 export const intro = localFont({
   src: [
-    { path: "/fonts/intro/Intro-Light.woff2", weight: "300", style: "normal" },
-    { path: "/fonts/intro/Intro-Regular.woff2", weight: "400", style: "normal" },
-    { path: "/fonts/intro/Intro-Book.woff2", weight: "500", style: "normal" },
+    { path: "../public/fonts/intro/Intro-Light.woff2", weight: "300", style: "normal" },
+    { path: "../public/fonts/intro/Intro-Regular.woff2", weight: "400", style: "normal" },
+    { path: "../public/fonts/intro/Intro-Book.woff2", weight: "500", style: "normal" },
     // Bold solo está disponible en .woff, lo incluimos como fallback.
-    { path: "/fonts/intro/Intro-Bold.woff", weight: "700", style: "normal" },
-    { path: "/fonts/intro/Intro-Black.woff2", weight: "900", style: "normal" },
+    { path: "../public/fonts/intro/Intro-Bold.woff", weight: "700", style: "normal" },
+    { path: "../public/fonts/intro/Intro-Black.woff2", weight: "900", style: "normal" },
   ],
   variable: "--font-body",
   display: "swap",
@@ -16,8 +16,8 @@ export const intro = localFont({
 
 export const introHead = localFont({
   src: [
-    { path: "/fonts/intro/Intro-Bold.woff", weight: "700", style: "normal" },
-    { path: "/fonts/intro/Intro-Black.woff2", weight: "900", style: "normal" },
+    { path: "../public/fonts/intro/Intro-Bold.woff", weight: "700", style: "normal" },
+    { path: "../public/fonts/intro/Intro-Black.woff2", weight: "900", style: "normal" },
   ],
   variable: "--font-head",
   display: "swap",
